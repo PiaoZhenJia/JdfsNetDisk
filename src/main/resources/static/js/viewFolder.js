3912df5d11ae3d18e0c00aecd1e7027a14da3fbd
朴哥哥 <623106882@qq.com>
2021-08-23 15:00:57 +0800

    0823

--- src/main/resources/static/js/viewFolder.js
+++ src/main/resources/static/js/viewFolder.js
@@ -33,45 +33,54 @@ function refreshTable() {
         success: function (res) {
             let html = "";
             res.dataValue.forEach(e => {
-                let line = "<tr>"
+                let line = "<div class='trDiv'>"
                 let folderIco = '<img src="/ico/folder.png">'
                 let fileIco = '<img src="/ico/file.png">'
                 if (e.isFolder == true) {//文件夹模式
-                    line += "<td "
+                    line += "<div class='tdLeftDiv '"
                     line += "onclick='clickFolder(\"" + e.name + "\")'"
                     line += ">"
                     line += folderIco
                     line += e.name
                     line += "(" + e.length + "个文件)"
-                    line += "</td>"
+                    line += "</div>"
                 } else {
-                    line += "<td "
+                    line += "<div class='tdLeftDiv' "
                     line += "onclick='clickFile(\"" + e.name + "\")'"
                     line += ">"
                     line += fileIco
                     line += e.name
                     line += "大小:" + e.length + "b"
-                    line += "</td>"
+                    line += "</div>"
                 }
                 //todo 第二个td
-                line += "<td>操作</td>"
+                line += "<div class='tdRightDiv' >操作</div>"
 
-                line += "</tr>"
+                line += "</div>"
                 html += line
             })
-            $("tbody").html(html)
+            $("#tableDiv").html(html)
+            refreshTableHeight()
         }
     })
 }
 
+function refreshTableHeight(){
+    //alert(($("#outSideDiv").height() - $("#uriLine").outerHeight()))
+    //$("#tableDiv").css("height",100)
+    $("#tableDiv").css("height","calc(" + ($("#outSideDiv").height() - $("#uriLine").outerHeight()) + "px - 5rem)")
+}
+
 function refreshUri() {
     let img = '<img src="/ico/right.png">'
-    let html = '<div class="btn" onclick=\'resetUri("/")\'>' + parent.getPageState() + '</div>'
+    let html = '<div class="btn uriBtn" id="toDesktop">' + '主页' + '</div>'
+    html += img
+    html += '<div class="btn uriBtn" onclick=\'resetUri("/")\'>' + parent.getPageState() + '</div>'
     let temp = ''
     for (let i = 0; i < uri.length; i++) {
         if (uri.charAt(i) == '/') {
             if (temp != '') {
-                html += '<div class="btn" onclick=\'resetUri("' + uri.substr(0, (i + 1)) + '")\'>' + temp + '</div>'
+                html += '<div class="btn uriBtn" onclick=\'resetUri("' + uri.substr(0, (i + 1)) + '")\'>' + temp + '</div>'
                 temp = ''
             }
             if (i < uri.length - 2) {
